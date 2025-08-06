ba07ae880f4f9a73a4fc44dd86b7863eb64ec79f
Shrikar32 <shrikarmujumdar32@gmail.com>
2025-08-06 23:04:55 +0530

    Commit1

---
+++ PrintingArrayElements.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+  cout << cars[0];
+  cout << cars[1];
+  cout << cars[2];
+  return 0;
+}
