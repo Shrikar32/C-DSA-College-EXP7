fb6118f78c349ec1f693423f55d437926668e40a
Shrikar32 <shrikarmujumdar32@gmail.com>
2025-08-06 23:38:09 +0530

    Commit 5

---
+++ concatenation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string first = "Hello";
+  string second = "World";
+  string result = first + " " + second;  // Concatenation
+
+  cout << "Result: " << result << endl;
+  return 0;
+}
